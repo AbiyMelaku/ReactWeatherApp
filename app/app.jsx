069a54926fd25a0f26fa42de0fa00b01db9f9469
Abiy Melaku <agirma08@gmail.com>
2017-08-17 22:07:10 -0700

    Create and link up About component

--- app/app.jsx
+++ app/app.jsx
@@ -6,12 +6,14 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 //requiring our components here
 var Main = require('Main');
 var Weather = require('Weather');
+var About = require('About');
 
 
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
+      <Route path="about" component={About}/>
       <IndexRoute component={Weather}/>
     </Route>
   </Router>,
