d1a3b77135b74b4e2273d5e7a651790873317d2e
Abiy Melaku <agirma08@gmail.com>
2017-08-17 21:59:38 -0700

    Require in Weather component
    add weather into react router with Index router

--- app/app.jsx
+++ app/app.jsx
@@ -5,13 +5,14 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 //var Router = require('react-router').Route;
 //requiring our components here
 var Main = require('Main');
+var Weather = require('Weather');
 
 
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
-    
+      <IndexRoute component={Weather}/>
     </Route>
   </Router>,
   document.getElementById('app')
