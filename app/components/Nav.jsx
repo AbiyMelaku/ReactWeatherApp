455da1566f0264dfb1f910c9f8884df15736c0a5
Abiy Melaku <agirma08@gmail.com>
2017-08-17 22:16:50 -0700

    Create Examples component
    created Examples component file
    added the examples component to react router in app file
    added the correct link to the nav bar
    modified webpack to have the right alias

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,11 +3,12 @@ var {Link} = require('react-router');
 
 var Nav = React.createClass({
   render: function(){
-    return(
+    return (
       <div>
         <h2>Nav Component</h2> 
         <Link to='/'>Get Weather</Link> 
         <Link to='/about'>About</Link> 
+        <Link to='/examples'>Examples</Link> 
       </div>
     )
   }
