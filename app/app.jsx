f7240416f63d83f474a36e2019a083cfc08a25f3
Abiy Melaku <agirma08@gmail.com>
2017-08-17 20:34:10 -0700

    Add react router + other libraries for weatherapp

---
+++ app/app.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+//ES6 destructuring syntax
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+//var Router = require('react-router').Route;
+
+
+
+ReactDOM.render(
+  <h1> boilerplate app</h1>,
+  document.getElementById('app')
+);
