7bbab1942f5f028968fa7714e521985ce0ae181b
Abiy Melaku <agirma08@gmail.com>
2017-08-17 21:21:57 -0700

    create main component

---
+++ app/components/main.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var Main = React.createClass({
+  render: function () {
+    return (
+      
+      //Navigation
+      
+      <h2>Main Component</h2>
+    );
+  }
+});
+
+module.exports = Main;
