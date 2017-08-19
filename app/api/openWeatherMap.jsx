48560dd235a5eb561ad502df4aca85c628c6cb4b
Abiy Melaku <agirma08@gmail.com>
2017-08-19 11:33:42 -0700

    Change h3 heading

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,20 +1,22 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://samples.openweathermap.org/data/2.5/weather?appid=1d412c3ae79dfe87b3a8eade7cac0854&units=imperial';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=1d412c3ae79dfe87b3a8eade7cac0854&units=imperial';
 
 module.exports = {
   getTemp: function(location){
     var encodedLocation = encodeURIComponent(location);
     var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
     
-    return axios.get(requestUrl).then(function(res){
-      if( res.data.cod && res.data.message ){
-        throw new Error(res.data.message);
-      } else {
-        return res.data.main.temp;
-      }
-    }, function(res){
-      throw newError(res.data.message);
-    });
+    return axios.get(requestUrl)
+      .then(function(res){
+        if(res.data.cod && res.data.message){
+          throw new Error('Unable to fetch weather');
+        } else {
+          return res.data.main.temp;
+        }
+      })
+      .catch(function(err){
+        throw new Error(err.res.data.meesage);
+      });
   }
 }
