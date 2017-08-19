18aaa23aa63f6efb2fbedab64ab190afac80e655
Abiy Melaku <agirma08@gmail.com>
2017-08-19 00:40:54 -0700

    Expanded Weather component
    add handleSubmit
    add getInitialState for default values
    pass props into children components

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -3,14 +3,31 @@ var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
 
 var Weather = React.createClass({
+  getInitialState: function(){
+    return {
+      location: 'Miami',
+      temp: 88
+    }
+  },
+  
+  handleSearch: function(location) {
+    //alert(location);
+    this.setState({
+      location: location,
+      temp: 23
+    })
+  },
+  
   render: function () {
-    return (
+      // var location = this.state.location;
+      // var temp = this.state.temp;
+      var {location, temp} = this.state;
+    return ( 
       <div>
         <h3> Weather component</h3>
-        <WeatherForm/>
-        <WeatherMessage/>
+        <WeatherForm onSearch={this.handleSearch}/>
+        <WeatherMessage location={location} temp={temp}/>
       </div>
-      
     )
   }
 });
