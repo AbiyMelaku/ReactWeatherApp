81a65f21fdcd6b9d885430bbe823ca29c8c1b30e
Abiy Melaku <agirma08@gmail.com>
2017-08-19 00:45:41 -0700

    Pass props to WeatherMessage component
    pass the location and temp values from the props from
    parent component (Weather) to child component (WeatherMessage)

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,8 +2,11 @@ var React = require('react');
 
 var WeatherMessage = React.createClass({
   render: function () {
+    // var location = this.props.location;
+    // var temp = this.props.temp;
+    var {temp, location} = this.this.props;
     return (
-      <h3> It is 40C in San Francisco</h3>
+      <h3> It is {temp} in {location}.</h3>
     )
   }
 });
