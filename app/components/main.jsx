5546bbf2733cb900b1e2c36f95dcd6a63cdb4d4e
Abiy Melaku <agirma08@gmail.com>
2017-08-19 14:43:04 -0700

    Convert main component to stateless functional
    use ES6

--- app/components/main.jsx
+++ app/components/main.jsx
@@ -2,17 +2,14 @@ var React = require('react');
 
 var Nav = require('Nav');
 
-var Main = React.createClass({
-  render: function () {
-    return (
-      //Navigation
-      <div>
-        <h2>Main Component</h2>
-        <Nav/>
-        {this.props.children}
-      </div>
-    );
-  }
-});
+var Main = (props) => {
+  return (
+    <div>
+      <h2>Main Component</h2>
+      <Nav/>
+      {props.children}
+    </div>
+  )
+}
 
 module.exports = Main;
