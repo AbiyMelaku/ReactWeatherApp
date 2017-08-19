bf26b0099d463258a96069465f058d44cefb598f
Abiy Melaku <agirma08@gmail.com>
2017-08-19 12:05:33 -0700

    Add isLoading component and wired correctly

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -6,36 +6,51 @@ var openWeatherMap = require('openWeatherMap');
 var Weather = React.createClass({
   getInitialState: function(){
     return {
-      location: 'Miami',
-      temp: 88
+      isLoading: false
     }
+    // return {
+    //   location: 'Miami',
+    //   temp: 88
+    // }
   },
   
   handleSearch: function(location) {
     var that = this;
+    
+    this.setState({
+      isLoading: true
+    });
     openWeatherMap.getTemp(location).then(function(temp){
       that.setState({
         location: location,
-        temp: temp
+        temp: temp,
+        isLoading: false
       });
     }, function(errorMessage){ 
       alert(errorMessage);
+      that.setState({
+        isLoading: false
+      })
     });
-    // this.setState({
-    //   location: location,
-    //   temp: 23
-    // })
   },
   
   render: function () {
       // var location = this.state.location;
       // var temp = this.state.temp;
-      var {location, temp} = this.state;
+      var {location, temp, isLoading} = this.state;
+      
+      function renderMessage(){
+        if( isLoading ){
+          return <h3>Fetching Weather...</h3>;
+        } else if( temp && location ){
+          return <WeatherMessage location={location} temp={temp}/>
+        }
+      }
     return ( 
       <div>
         <h3> Weather component</h3>
         <WeatherForm onSearch={this.handleSearch}/>
-        <WeatherMessage location={location} temp={temp}/>
+        {renderMessage()}
       </div>
     )
   }
