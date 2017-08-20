f913e52fb1886b6600baa967de7b87d6a4994174
Abiy Melaku <agirma08@gmail.com>
2017-08-20 15:30:04 -0700

    style about page and gave some links to tools

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,12 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3> About component </h3>
-      <p3> Welcome to the about component </p3>
+      <h3 className="text-center">About</h3>
+      <p>Welcome to my simple weather application written in <a href="https://facebook.github.io/react">React.js!</a> </p>
+      
+      <p>The API is <a href="http://openweathermap.org/">Open Weather Map</a>.
+      Sign up is free and easy! </p>
+      
     </div>
   )
 }
