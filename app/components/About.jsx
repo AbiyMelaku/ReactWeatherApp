ce3556bcaaabad6d298e773cf46edd97f2e18ec7
Abiy Melaku <agirma08@gmail.com>
2017-08-19 20:33:45 -0700

    changes for heroku deployment

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,7 +1,12 @@
 var React = require('react');
 
 var About = (props) => {
-  return <h3> About component</h3>
+  return (
+    <div>
+      <h3> About component </h3>
+      <p3> Welcome to the about component </p3>
+    </div>
+  )
 }
 
 module.exports = About;
