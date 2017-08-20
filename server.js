6b50ff0c0b6c3ec061b743bd4e6f6e8ca52a9d94
Abiy Melaku <agirma08@gmail.com>
2017-08-19 19:59:08 -0700

    Prepare for Heroku deployment

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ var express = require('express');
 
 //create the app
 var app = express();
+const PORT = process.env || 3000;
+
+app.use(function(req, res, next){
+  if( req.headers['x-forwarded-proto'] === 'http' ){
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-  console.log('Server is up and listening at port 3000')
+app.listen(port, function(){
+  console.log('Server is up and listening at port ' + PORT);
 })
